1f9a71079e4a79be0f759ab3899d204bb15ee226
Prince Asamoah <prince.asamoah@amalitech.com>
2025-09-04 11:32:51 +0000

    write integration test for withdraw_amount action

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -10,6 +10,7 @@ class TestActions(unittest.TestCase):
         self.account = BankAccount()
         
     def tearDown(self) -> None:
+        """Cleanup bank account"""
         del self.account
     
     @patch('builtins.input', return_value='100')
@@ -20,3 +21,13 @@ class TestActions(unittest.TestCase):
         self.assertEqual(self.account.balance, 100)
         self.assertIn('Deposited $100.0.', mock_stdout.getvalue())
         self.assertIn('New Balance: $100.0', mock_stdout.getvalue())
+        
+    @patch('builtins.input', return_value='50')
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_withdraw_amount(self, mock_stdout, mock_input):
+        self.account.add_amount(100) # preload balance
+        actions.withdraw_amount(self.account)
+        mock_input.assert_called_once_with('Enter amount to withdraw: ')
+        self.assertEqual(self.account.balance, 50)
+        self.assertIn('Withdrawn $50.0.', mock_stdout.getvalue())
+        self.assertIn('New Balance: $50.0', mock_stdout.getvalue())
