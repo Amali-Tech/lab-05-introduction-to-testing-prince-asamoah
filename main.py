047ad66b421f6a4dc400180fc47e4b0139280fb3
Prince Asamoah <prince.asamoah@amalitech.com>
2025-09-04 08:34:12 +0000

    update main to create bank account

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# Main program starts here
+# A simple terminal-based banking app.
+# Allows the user to deposit and withdraw amount from a bank account.
+from banking import BankAccount
+
+def main():
+    """Banking application starts here"""
+    
+    # Create a new bank account
+    account = BankAccount()
+    
+    print(f'Current Balance: ${account.balance}')
+
+
+if __name__ == "__main__":
+    main()
