94f8a1f09304dffe108be39bee647b64c6c7825b
Prince Asamoah <prince.asamoah@amalitech.com>
2025-09-09 15:14:03 +0000

    write test to check raised exceptions

--- tests/test_banking.py
+++ tests/test_banking.py
@@ -19,6 +19,28 @@ class TestBankAcount(unittest.TestCase):
         self.account.withdraw_amount(50)
         self.assertEqual(self.account.balance, 50 )
         
+    def test_add_zero_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            self.account.add_amount(0)
+            
+    def test_add_negative_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw_amount(0)
+            
+    def test_withdraw_zero_raises_value_error(self):
+        self.account.add_amount(100)
+        with self.assertRaises(ValueError):
+            self.account.withdraw_amount(0)
+            
+    def test_withdraw_negative_raises_value_error(self):
+        self.account.add_amount(100)
+        with self.assertRaises(ValueError):
+            self.account.withdraw_amount(-10)
+            
+    def test_overdraft_raises_value_error(self):
+        self.account.add_amount(100)
+        with self.assertRaises(ValueError):
+            self.account.withdraw_amount(200)
 
 if __name__ == '__main__':
     unittest.main()
