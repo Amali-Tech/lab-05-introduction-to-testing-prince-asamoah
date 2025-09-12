9265cf2e54ae4fd6d9b967dc84546ce71ab0f992
Prince Asamoah <prince.asamoah@amalitech.com>
2025-09-12 15:15:43 +0000

    write test for generate summary

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -9,3 +9,17 @@ class TestGenerateSummary(unittest.TestCase):
     def tearDown(self) -> None:
         del self.account
         
+    def test_summary_after_deposit(self):
+        self.account.add_amount(100)
+        summary = self.account.generate_summary()
+        self.assertEqual(summary, 'Balance: $100.0, Last Transaction: +100.0')
+    
+    def test_summay_after_withdrawal(self):
+        self.account.add_amount(200)
+        self.account.withdraw_amount(50)
+        summary = self.account.generate_summary()
+        self.assertEqual(summary, 'Balance: $150.0, Last Transaction: -50.0' )
+        
+    def test_summary_after_initial_balance(self):
+        test_summary = self.account.generate_summary()
+        self.assertEqual(test_summary, 'Balance: $0.0, Last Transaction: None')
