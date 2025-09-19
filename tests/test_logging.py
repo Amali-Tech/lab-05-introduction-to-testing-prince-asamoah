6326305741f19a9a7b18e3df09275978d3cc39ae
Prince Asamoah <prince.asamoah@amalitech.com>
2025-09-19 06:53:15 +0000

    update tests for logger service

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,24 +1,45 @@
 import unittest
-from unittest.mock import Mock
-from logger import LoggerService
-from banking import BankAccount
+from io import StringIO
+from unittest.mock import patch
+from logger import LoggerService, LogLevel
 
-class TestBankAccountLogging(unittest.TestCase):
+class TestLogging(unittest.TestCase):
     def setUp(self) -> None:
-        """Setup mock logger before each test"""
-        self.mock_logger = Mock(spec=LoggerService)
-        self.account = BankAccount(logger=self.mock_logger)
+        self.logger = LoggerService(name="TestLogger")
+    
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_info_log(self, mock_stdout):
+        test_log_message = 'This is an info message'
+        self.logger.log(test_log_message, level=LogLevel.INFO)
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("[TestLogger]", output)
+        self.assertIn("[INFO]", output)
+        self.assertIn(test_log_message, output)
         
-    def tearDown(self) -> None:
-        """Reset mock logger and account"""
-        self.mock_logger.reset_mock()
-        del self.account
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_error_log(self, mock_stdout):
+        test_log_message = 'This is an error message'
+        self.logger.log(test_log_message, level=LogLevel.ERROR)
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("[TestLogger]", output)
+        self.assertIn("[ERROR]", output)
+        self.assertIn(test_log_message, output)
+        
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_debug_log(self, mock_stdout):
+        test_log_message = 'This is a debug message'
+        self.logger.log(test_log_message, level=LogLevel.DEBUG)
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("[TestLogger]", output)
+        self.assertIn("[DEBUG]", output)
+        self.assertIn(test_log_message, output)
     
-    def test_deposit_logs_transaction(self):
-        self.account.add_amount(100)
-        self.mock_logger.log.assert_called_once_with("Deposited $100")
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_warning_log(self, mock_stdout):
+        test_log_message = 'This is a warning message'
+        self.logger.log(test_log_message, level=LogLevel.WARNING)
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("[TestLogger]", output)
+        self.assertIn("[WARNING]", output)
+        self.assertIn(test_log_message, output)
     
-    def test_withdraw_logs_transaction(self):
-        self.account.add_amount(200)
-        self.account.withdraw_amount(50)
-        self.mock_logger.log.assert_called_with("Withdrew $50")
